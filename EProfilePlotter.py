47bef2a1d181e607ff193c94cdf5d78b68d26736
Hoimin Jung <hmjung95@kaist.ac.kr>
2021-04-21 14:07:32 +0900

    Energy plotter update

--- EProfilePlotter.py
+++ EProfilePlotter.py
@@ -33,7 +33,10 @@ class EProfilePlotterMainWindow(QMainWindow, form_class):
         self.btnAdd.pressed.connect(self.btnAddClicked)
         self.btnClear.pressed.connect(self.btnClearClicked)
         self.btnColor.pressed.connect(self.btnColorClicked)
+        self.btnDefault.pressed.connect(self.btnDefaultClicked)
         self.btnConvert.pressed.connect(self.btnConvertedClicked)
+        self.sliderCurveLevel.valueChanged.connect(self.changeValueCurveLevel)
+        self.sliderLineWidth.valueChanged.connect(self.changeValueLineWidth)
 
     @pyqtSlot()
     def btnPathClicked(self):
@@ -45,15 +48,13 @@ class EProfilePlotterMainWindow(QMainWindow, form_class):
                                 "Initial code by Dr. Florian Mulks (Google colab).\nRewritten in Python by Dr. Jinhoon Jeong.\nModified by Hoimin Jung and Mina Son @ Baikgroup KAIST",
                                 QMessageBox.Ok)
 
-    def btnAddClicked(self):
-        energy_seq = self.txtEnergySequence.text()
-        font_size = self.comboFontSize.currentText()
-        color_code = self.btnColor.styleSheet().split(':')[-1].strip()[0:7]
-        curve_level = self.comboCurveLevel.currentText()
-        width_ratio = self.comboWidthRatio.currentText()
+    def changeValueCurveLevel(self):
+        value = int(self.sliderCurveLevel.value())
+        self.txtCurveLevel.setText(str(value))
 
-        self.txtToConvert.appendPlainText(
-            "[" + energy_seq + "] " + font_size + " " + color_code + " " + curve_level + " " + width_ratio)
+    def changeValueLineWidth(self):
+        value = int(self.sliderLineWidth.value())
+        self.txtLineWidth.setText(str(float(value/10)))
 
     def btnClearClicked(self):
         self.txtToConvert.setPlainText("")
@@ -63,64 +64,108 @@ class EProfilePlotterMainWindow(QMainWindow, form_class):
         col_comp = QColor(255 - col.getRgb()[0], 255 - col.getRgb()[1], 255 - col.getRgb()[2])
         self.btnColor.setStyleSheet('QWidget{color: ' + col_comp.name() + '; background-color: ' + col.name() + ';}')
 
+    def btnDefaultClicked(self):
+        self.sliderCurveLevel.setValue(4)
+        self.sliderLineWidth.setValue(10)
+        self.comboFontSize.setCurrentIndex(3)
+        self.btnColor.setStyleSheet('QWidget{color:#ffffff;\nbackground-color: #000000;}')
+
+    def btnAddClicked(self):
+        energy_seq = self.txtEnergySequence.text()
+        font_size = self.comboFontSize.currentText()
+        color_code = self.btnColor.styleSheet().split(':')[-1].strip()[0:7]
+        curve_level = self.txtCurveLevel.text()
+        width_ratio = self.txtLineWidth.text()
+
+        #Energy sequence validity check
+        temp = energy_seq.replace("-", "")
+        temp = temp.replace(".", "")
+        temp = "".join(temp.strip().split())
+        if temp.isdigit():
+            self.txtToConvert.appendPlainText(
+                "[" + energy_seq + "] " + font_size + " " + color_code + " " + curve_level + " " + width_ratio)
+        else:
+            QMessageBox.information(self,"Notice", "Invalid energy sequence\nPlease write down numbers only.", QMessageBox.Ok)
+
     def btnConvertedClicked(self):
-        toConvert = self.txtToConvert.toPlainText().split("\n")
-        rootPath = self.txtPath.text()
-        filename = self.txtFileName.text()
-        filePath = rootPath + "/" + filename + ".cdxml"
+        if self.txtToConvert.toPlainText() == "":
+            QMessageBox.information(self, "Notice", "Please add energy sequences.", QMessageBox.Ok)
 
-        if not (os.path.isdir(Path(rootPath))):
-            os.makedirs(Path(rootPath))
+        else:
+            toConvert = self.txtToConvert.toPlainText().split("\n")
+            rootPath = self.txtPath.text()
+            filename = self.txtFileName.text()
+            filePath = rootPath + "/" + filename + ".cdxml"
 
-        #Get colors
-        colortable = []
-        for line in toConvert:
-            colortable.append(line.split("]")[1].split()[1])
+            if not (os.path.isdir(Path(rootPath))):
+                os.makedirs(Path(rootPath))
 
-        #Initiate cdxml_file & write header
-        writing_file(filePath, header(1,1, colortable), "w")
+            #File_exist check
+            if os.path.isfile(Path(filePath)):
+                reply = QMessageBox.question(self, "Notice", "Do you want to overwrite file "+filename+".cdxml?", QMessageBox.Yes|QMessageBox.No)
+                if reply == QMessageBox.Yes:
+                    pass
+                elif reply == QMessageBox.No:
+                    while True:
+                        temp = 1
+                        filename = filename.replace(filename, filename+"_"+str(temp))
+                        filePath = rootPath + "/" + filename + ".cdxml"
+                        if not os.path.isfile(Path(filePath)):
+                            break
 
-        y_size_list = 0.0
-        #Write cdxml file
-        for line in toConvert:
-            try:
-                curr_energy = list(map(float, line.split("]")[0].split("[")[1].split()))
-                width = line.split("]")[1].split()[3]
-                curve = line.split("]")[1].split()[2]
-                font_size = int(line.split("]")[1].split()[0])
-                rel_dx = float(width)
-                rel_curv = float(curve) / 4
-                #generate_curve(filePath, curr_energy, width, curve, colortable, font_size)
+            #Get colors
+            colortable = []
+            for line in toConvert:
+                colortable.append(line.split("]")[1].split()[1])
 
-                # starting point
-                max_val, min_val = max(curr_energy), min(curr_energy)
-                starting_point = [70, 50 + ((max_val - curr_energy[0] + y_size_list) * 5) + y_size_list]
-                y_size_list += (max_val - min_val) + 10
+            #Initiate cdxml_file & write header
+            writing_file(filePath, header(1,1, colortable), "w")
 
-                # setting for pages
-                #if (len(curr_energy) - 1) * 20 * rel_dx > 440: page_W = 2
-                #if diff_val * 5 + 70 > 750: page_H = 2
+            y_size_list = 0.0
+            #Write cdxml file
+            for i in range(len(toConvert)):
+                line = toConvert[i]
+                try:
+                    curr_energy = list(map(float, line.split("]")[0].split("[")[1].split()))
+                    width = line.split("]")[1].split()[3]
+                    curve = line.split("]")[1].split()[2]
+                    font_size = int(line.split("]")[1].split()[0])
+                    rel_dx = float(width)
+                    rel_curv = float(curve) / 4
 
-                #make labels
-                curr_labels = []
-                for i in range(len(curr_energy)):
-                    if i % 2 == 0:
-                        temp_lb = "%d" % (i // 2 + 1)
-                    else:
-                        temp_lb = "%d-TS" % (i // 2 + 1)
-                    curr_labels.append(temp_lb)
+                    # starting point
+                    max_val, min_val = max(curr_energy), min(curr_energy)
+                    starting_point = [70, 50 + ((max_val - curr_energy[0] + y_size_list) * 5) + y_size_list]
+                    y_size_list += (max_val - min_val) + 10
 
-                points_Eprofile = make_points_Eprofile(curr_energy, starting_point, rel_dx)
-                curve_points = make_curve_points(points_Eprofile, rel_curv)
-                make_dots(filePath, points_Eprofile)
-                make_curve(filePath, curve_points)
-                make_labels(filePath, curr_energy, curr_labels, points_Eprofile, font_size)
+                    # setting for pages
+                    #if (len(curr_energy) - 1) * 20 * rel_dx > 440: page_W = 2
+                    #if diff_val * 5 + 70 > 750: page_H = 2
 
-            except:
-                continue
+                    #make labels
+                    curr_labels = []
+                    for j in range(len(curr_energy)):
+                        if j % 2 == 0:
+                            temp_lb = "%d" % (j // 2 + 1)
+                        else:
+                            temp_lb = "%d-TS" % (j // 2 + 1)
+                        curr_labels.append(temp_lb)
 
-        #Close file
-        writing_file(filePath, "</page></CDXML>", "a")
+                    color_index = i+4
+                    points_Eprofile = make_points_Eprofile(curr_energy, starting_point, rel_dx)
+                    curve_points = make_curve_points(points_Eprofile, rel_curv)
+                    make_dots(filePath, points_Eprofile, color_index)
+                    make_curve(filePath, curve_points, color_index)
+                    make_labels(filePath, curr_energy, curr_labels, points_Eprofile, font_size, color_index)
+
+                except:
+                    continue
+
+            #Close file
+            writing_file(filePath, "</page></CDXML>", "a")
+
+            #Open folfer
+            os.startfile(Path(filePath).parent)
 
 def writing_file(filePath, contents, type):
     f = open(filePath, type)
@@ -186,19 +231,19 @@ def make_curve_points(points_Eprofile, rel_curv):
         curve_points = curve_points + [xy[0] - 15 * rel_curv, xy[1], xy[0], xy[1], xy[0] + 15 * rel_curv, xy[1]]
     return " ".join(str(item) for item in curve_points)
 
-def make_curve(filename, curve_points):
+def make_curve(filename, curve_points, color_index):
     f = open(filename, "a")
-    f.write('<curve\n id="4"\n Z="1"\n ArrowheadType="Solid"\n CurvePoints="%s"\n/>\n' % (curve_points))
+    f.write('<curve\n id="4"\n Z="1"\n color=\"%s\"\nArrowheadType="Solid"\n CurvePoints="%s"\n/>\n' % (str(color_index), curve_points))
     f.close()
 
-def make_dots(filename, points_Eprofile):
+def make_dots(filename, points_Eprofile, color_index):
     f = open(filename, "a")
     for xy in points_Eprofile:
-        f.write('<graphic\n BoundingBox="%s %s %s %s"\n GraphicType="Symbol"\n SymbolType="Electron"\n/>\n' % (
-        xy[0], xy[1], xy[0], xy[1] + 15))
+        f.write('<graphic\n BoundingBox="%s %s %s %s"\n color=\"%s\"\nGraphicType="Symbol"\n SymbolType="Electron"\n/>\n' % (
+        xy[0], xy[1], xy[0], xy[1] + 15, str(color_index)))
     f.close()
 
-def make_labels(filename, values, labels, points_Eprofile, font_size):
+def make_labels(filename, values, labels, points_Eprofile, font_size, color_index):
     f = open(filename, "a")
     if font_size > 9:
         alpha = font_size / 4
@@ -211,54 +256,14 @@ def make_labels(filename, values, labels, points_Eprofile, font_size):
             y_inc = 12 + alpha
         f.write('<t\n p="%s %s"\n CaptionJustification="Center"\n Justification="Center"\n LineHeight="auto"\n>' % (
         xy[0], xy[1] + y_inc))
-        f.write('<s font="3" size="{0}" color="0" face="1">{1}\n</s>'.format(font_size, labels[i]))
+        f.write('<s font="3" size="{0}" color="{1}" face="1">{2}\n</s>'.format(font_size, str(color_index), labels[i]))
         if float(values[i]) < 0:  # make en-dash
             num = str(values[i]).split("-")[1]
-            f.write('<s font="3" size="{0}" color="0">(</s>'.format(font_size))
-            f.write('<s font="7" size="{0}" color="0">-</s><s font="3" size="{1}" color="0">{2:0.2f})</s></t>\n'.format(font_size,font_size,float(num)))
+            f.write('<s font="3" size="{0}" color="{1}">(</s>'.format(font_size, str(color_index)))
+            f.write('<s font="7" size="{0}" color="{1}">-</s><s font="3" size="{2}" color="0">{3:0.2f})</s></t>\n'.format(font_size,str(color_index),font_size,float(num)))
         else:
-            f.write('<s font="3" size="{0}" color="0">({1:0.2f})</s></t>\n'.format(font_size, float(values[i])))
+            f.write('<s font="3" size="{0}" color="{1}">({2:0.2f})</s></t>\n'.format(font_size, str(color_index), float(values[i])))
     f.close()
-
-
-def initialize_file(filePath, colortable):
-    cdxml_file = open(filePath, 'w')
-
-    #General info write
-    cdxml_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
-                    + " <!DOCTYPE CDXML SYSTEM \"http://www.cambridgesoft.com/xml/cdxml.dtd\" >\n"
-                    + " <CDXML><fonttable>\n" + "<font id=\"3\" charset=\"iso-8859-1\" name=\"Arial\"/>\n<font id=\"7\" charset=\"Unknown\" name=\"Symbol\"/>\n"
-                    + "</fonttable>\n")
-
-    #color info
-    cdxml_file.write("<colortable>\n<color r=\"1\" g=\"1\" b=\"1\"/>\n<color r=\"0\" g=\"0\" b=\"0\"/>\n")
-    for color in colortable:
-        col = QColor(color)
-        col_r = int(col.getRgb()[0])
-        col_g = int(col.getRgb()[1])
-        col_b = int(col.getRgb()[2])
-        cdxml_file.write("<color r=\"" + str(round(col_r / 255, 2)) + "\" g=\"" + str(round(col_g / 255,2)) + "\" b=\"" + str(round(col_b / 255, 2)) + "\"/>\n")
-    cdxml_file.write("</colortable>")
-
-    # Page setup
-    cdxml_file.write("<page\n"
-                     + " id=\"6\"\n"
-                     + " BoundingBox=\"0 0 523.20 769.68\"\n"
-                     + " HeaderPosition=\"36\"\n"
-                     + " FooterPosition=\"36\"\n"
-                     + " PrintTrimMarks=\"yes\"\n"
-                     + " HeightPages=\"1\"\n"
-                     + " WidthPages=\"1\"\n"
-                     + ">\n")
-
-    Arrow = "<arrow\n" + " id=\"13\"\n" + " BoundingBox=\"13.87 13.84 18.63 60\"\n" + " Z=\"6\"\n" + " LineWidth=\"0.85\"\n" + " FillType=\"None\"\n" + " ArrowheadHead=\"Full\"\n" + " ArrowheadType=\"Solid\"\n" + " HeadSize=\"1000\"\n" + " ArrowheadCenterSize=\"875\"\n" + " ArrowheadWidth=\"250\"\n" + " Head3D=\"16.50 13.84 0\"\n" + " Tail3D=\"16.50 64 0\"\n" + " Center3D=\"21.75 45.59 0\"\n" + " MajorAxisEnd3D=\"71.91 45.59 0\"\n" + " MinorAxisEnd3D=\"21.75 95.75 0\"\n" + "/>"
-
-    G_sol = "<t\n" + " id=\"11\"\n" + " p=\"40.50 24.50\"\n" + " BoundingBox=\"20.99 15.45 60.01 37.65\"\n" + " Z=\"7\"\n" + " CaptionJustification=\"Center\"\n" + " Justification=\"Center\"\n" + " LineHeight=\"auto\"\n" + " LineStarts=\"8 18\"\n" + "><s font=\"7\" size=\"9\" color=\"0\" face=\"1\">D</s><s font=\"3\" size=\"9\" color=\"0\" face=\"1\">G(sol)\n" + "</s><s font=\"3\" size=\"9\" color=\"0\">(kcal/mol)</s></t>"
-
-    cdxml_file.write(Arrow)
-    cdxml_file.write(G_sol)
-
-    cdxml_file.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
